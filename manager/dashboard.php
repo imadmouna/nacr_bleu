c2a6960a78f450ea01e95d05c05ed8cb60b258af
unknown <imad@gmail.fr>
2015-09-30 14:52:11 +0000

    wip layout

--- manager/dashboard.php
+++ manager/dashboard.php
@@ -67,10 +67,10 @@ include("../connect.php");
         <section class="content-header">
           <h1>
             Dashboard
-            <small>Control panel</small>
+            <small>Panneau de controle</small>
           </h1>
           <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li><a href="#"><i class="fa fa-dashboard"></i> Accueil</a></li>
             <li class="active">Dashboard</li>
           </ol>
         </section>
@@ -145,8 +145,8 @@ include("../connect.php");
     <script src='plugins/fastclick/fastclick.min.js'></script>
     <!-- AdminLTE App -->
     <script src="dist/js/app.min.js" type="text/javascript"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="dist/js/demo.js" type="text/javascript"></script>
+    <!-- AdminLTE for demo purposes 
+    <script src="dist/js/demo.js" type="text/javascript"></script>-->
     <script src="plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>
     <!-- CK Editor -->
     <script src="//cdn.ckeditor.com/4.4.3/standard/ckeditor.js"></script>
